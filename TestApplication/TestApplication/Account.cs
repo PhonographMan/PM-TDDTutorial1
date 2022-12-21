2336d2c3a299e87ba79adf2fd4abe871d003819c
Scott Foster <scottgarryfoster@googlemail.com>
2022-12-21 18:59:38 +0000

    Added account with balance and overdraft.

---
+++ TestApplication/TestApplication/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    public class Account
+    {
+        public int Balance 
+        {
+            get => balance; 
+            set 
+            {
+                balance = ReturnHighestSafeBalance(balance, value);
+            }
+        }
+
+        private int balance;
+
+        public int Overdraft;
+
+        private int ReturnHighestSafeBalance(int existingValue, int newValue)
+        {
+            int safeValue = existingValue;
+            if (newValue >= -Overdraft)
+            {
+                safeValue = newValue;
+            }
+
+            return safeValue;
+        }
+    }
+}
---
+++ TestApplication/TestApplicationTests/AccountTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using TestApplication;
+
+namespace TestApplicationTests
+{
+    [TestClass]
+    public class AccountTests
+    {
+        [TestMethod]
+        public void Balance_ReturnsZero_WhenANewAccountIsCreatedTest()
+        {
+            // Arrange
+            var newAccount = new Account();
+
+            // Act
+            int balance = newAccount.Balance;
+
+            // Assert
+            Assert.AreEqual(0, balance);
+        }
+
+        [TestMethod]
+        public void Overdraft_ReturnsZero_WhenANewAccountIsCreatedTest()
+        {
+            // Arrange
+            var newAccount = new Account();
+
+            // Act
+            int overdraft = newAccount.Overdraft;
+
+            // Assert
+            Assert.AreEqual(0, overdraft);
+        }
+
+        [TestMethod]
+        public void Balance_ReturnsZero_WhenOverdraftAndBalanceAreZeroAnd100IsTakenTest()
+        {
+            // Arrange
+            var newAccount = new Account();
+
+            // Act
+            newAccount.Balance -= 100;
+            int balance = newAccount.Balance;
+
+            // Assert
+            Assert.AreEqual(0, balance);
+        }
+
+        [TestMethod]
+        public void Balance_Returns100_When100IsAddedToABlankBalanceTest()
+        {
+            // Arrange
+            var newAccount = new Account();
+
+            // Act
+            newAccount.Balance += 100;
+            int balance = newAccount.Balance;
+
+            // Assert
+            Assert.AreEqual(100, balance);
+        }
+
+        [TestMethod]
+        public void Balance_ReturnsMinus100_WhenBalanceIsZeroAndOverdraftIs100And100IsTakenFromBalanceTest()
+        {
+            int expectedValue = -100;
+
+            // Arrange
+            var newAccount = new Account();
+            newAccount.Overdraft = 100;
+
+            // Act
+            newAccount.Balance -= 100;
+            int balance = newAccount.Balance;
+
+            // Assert
+            Assert.AreEqual(expectedValue, balance);
+        }
+    }
+}
