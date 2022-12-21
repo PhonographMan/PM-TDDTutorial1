e8703c492a2d123e294ec3d9ec86754a9fb6f9ab
Scott Foster <scottgarryfoster@googlemail.com>
2022-12-21 19:06:56 +0000

    Cleaning code a little.

--- TestApplication/TestApplication/Account.cs
+++ TestApplication/TestApplication/Account.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace TestApplication
 {
+    /// <summary>
+    /// Bank account for a given user.
+    /// </summary>
     public class Account
     {
+        /// <summary>
+        /// Balance for the account. Is kept in check by <see cref="Overdraft"/>
+        /// </summary>
         public int Balance 
         {
             get => balance; 
@@ -16,11 +22,21 @@ namespace TestApplication
                 balance = ReturnHighestSafeBalance(balance, value);
             }
         }
-
         private int balance;
 
+        /// <summary>
+        /// Overdraft for the account. Allows <see cref="Balance"/> to decend below zero.
+        /// </summary>
         public int Overdraft;
 
+        /// <summary>
+        /// Ensures value is not below <see cref="Overdraft"/>
+        /// </summary>
+        /// <param name="existingValue"> Value started at. </param>
+        /// <param name="newValue"> Proposed value. </param>
+        /// <returns> 
+        /// A value no lower than <see cref="Overdraft"/>. May equal <see cref="Overdraft"/> if proposed is below.
+        /// </returns>
         private int ReturnHighestSafeBalance(int existingValue, int newValue)
         {
             int safeValue = existingValue;
