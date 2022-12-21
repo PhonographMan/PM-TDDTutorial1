c9adef19fcd054bdd19a3de2341a9a5d9800a0ff
Scott Foster <scottgarryfoster@googlemail.com>
2022-12-21 16:44:03 +0000

    Blank Project
    Added Blank Project.

---
+++ TestApplication/TestApplicationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace TestApplicationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
